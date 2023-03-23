59b8431e5a6a7c7027f05de712534cb16ba5fd5d
Addisalm_Seifu <addisalemseifu@gmail.com>
2023-03-23 11:38:02 +0300

    restructure functions in to methods and added class

--- script.js
+++ script.js
@@ -2,38 +2,40 @@ const addBooks = document.querySelector('.addBook');
 const booksList = document.querySelector('.books-list');
 const remover = document.getElementsByClassName('remover');
 const newB = document.getElementsByClassName('thebook');
-function Book(name, title) {
-  this.name = name;
-  this.title = title;
-  this.id = Math.random();
-}
 const newBooks = JSON.parse(localStorage.getItem('mydata')) || [];
 
-function bookAdder() {
-  newBooks.forEach((book) => {
-    booksList.innerHTML += `<div class="newBook thebook">
-      <h2 class="name">"${book.name}" by &nbsp;</h2>
-      <h2 class="title">${book.title}</h2>
-      <button class="remover" id=${book.id}>Remove</button>
-  </div>`;
-  });
-  // eslint-disable-next-line no-plusplus
-  for (let i = 0; i < newB.length; i++) {
-    if (i % 2 !== 0) {
-      newB[i].classList.add('backgroundColor');
+class Book {
+  constructor(name, title) {
+    this.name = name;
+    this.title = title;
+    this.id = Math.random();
+  }
+
+  static bookAdder() {
+    newBooks.forEach((book) => {
+      booksList.innerHTML += `<div class="newBook thebook">
+        <h2 class="name">"${this.name}" by &nbsp;</h2>
+        <h2 class="title">${this.title}</h2>
+        <button class="remover" id=${book.id}>Remove</button>
+    </div>`;
+    });
+    for (let i = 0; i < newB.length; i++) {
+      if (i % 2 !== 0) {
+        newB[i].classList.add('backgroundColor');
+      }
     }
   }
-}
 
-function bookCreator() {
-  const inputTitle = document.querySelector('.txtTitle');
-  const inputAuthor = document.querySelector('.txtAuthor');
-  const bookFunction = new Book(inputAuthor.value, inputTitle.value);
-  newBooks.push(bookFunction);
-  localStorage.setItem('mydata', JSON.stringify(newBooks));
-  booksList.innerHTML = '';
-  inputTitle.value = '';
-  inputAuthor.value = '';
+  static bookCreator() {
+    const inputTitle = document.querySelector('.txtTitle');
+    const inputAuthor = document.querySelector('.txtAuthor');
+    const bookFunction = new Book(inputAuthor.value, inputTitle.value);
+    newBooks.push(bookFunction);
+    localStorage.setItem('mydata', JSON.stringify(newBooks));
+    booksList.innerHTML = '';
+    inputTitle.value = '';
+    inputAuthor.value = '';
+  }
 }
 
 function bookRemover() {
@@ -51,12 +53,12 @@ function bookRemover() {
   }
 }
 
-bookAdder();
+Book.bookAdder();
 bookRemover();
 
 addBooks.addEventListener('click', (e) => {
   e.preventDefault();
-  bookCreator();
-  bookAdder();
+  Book.bookCreator();
+  Book.bookAdder();
   bookRemover();
 });
